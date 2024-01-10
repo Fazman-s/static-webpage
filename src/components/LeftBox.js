3efb763ca7b0e39169256e468c14233472b57cdc
Fazman-s <fazmansam@gmail.com>
2024-01-10 22:23:40 +0530

    Initial commit

--- src/components/LeftBox.js
+++ src/components/LeftBox.js
@@ -4,7 +4,7 @@ import React from 'react';
 const LeftBox = () => {
   return (
     <div className="left-box">
-      <img src="fazman.png" height={100} width={100} alt="Your Profile" />
+      <img src="Fazman.jpg" height={100} width={100} alt="Your Profile" />
     </div>
   );
 };
